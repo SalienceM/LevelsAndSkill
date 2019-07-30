9bec1a12133f9a680d96d9092a241bf947aeea92
muunnn <hackingmu@outlook.com>
2019-07-31 00:16:58 +0800

    完善分割算法,实现非重复全排列

--- src/main/java/com/m31/minecraft/forge/levelsandskill/utils/Splitor.java
+++ src/main/java/com/m31/minecraft/forge/levelsandskill/utils/Splitor.java
@@ -5,66 +5,77 @@ import java.util.List;
 
 public class Splitor {
 
-    public static List<List<String>> subListByCount(int targetCount,String[] seq){
-        List<List<String>> totalList=new ArrayList<>();
-        int k=0;
-        int topIndex=-1;
-       List<String> innerStrList=new ArrayList<String>();
-        for(int i=k;i<seq.length;i++){
-            if(topIndex==i)continue;
-            innerStrList.add(seq[i]);
-            if(innerStrList.size()==targetCount){
-                totalList.add(innerStrList);
-                innerStrList=new ArrayList<String>();
-                topIndex=i;
-                if(topIndex==seq.length-1){
-                    i=k++;
 
-                }else{
-                    i=k-1;
+    /**非重复顺序排列组合算法
+     *
+     * @param slot  位数
+     * @param args  当前
+     * @return
+     */
+    public static List<String> getCombies(int slot,String[] args){
+        if (null==args)return new ArrayList<>();
+        //获取全排列
+        List<String> list=new ArrayList<>();
+        for(int slotIndex=0;slotIndex<slot;slotIndex++){
+            if(list.isEmpty()){
+                for(int x=0;x<args.length;x++){
+                    list.add(String.valueOf(x));
                 }
-            }
-        }
-        return totalList;
-    }
-
-    public static List<List<String>> subListByCountV2(int targetCount,String[] seq){
-        List<List<String>> totalList=new ArrayList<>();
-        List<String> innerStrList=new ArrayList<String>();
-        int k=0;
-        int a=k+1;
-        for(int i=k;i<seq.length;i++){
-            if(i==seq.length-1&& a==i)break;
-            if(seq.length==a)break;
-            innerStrList.add(seq[i]);
-            if(innerStrList.size()==targetCount){
-                totalList.add(innerStrList);
-                innerStrList=new ArrayList<>();
-                continue;
-            }
-            for(int j=a;j<seq.length;j++){
-                innerStrList.add(seq[j]);
-                if(innerStrList.size()==targetCount){
-                    totalList.add(innerStrList);
-                    innerStrList=new ArrayList<>();
-                    if(j==seq.length-1){
-                        a=i+2;
-                        break;
-                    }else{
-                        i--;
-                        a++;
-                        break;
+            }else{
+                List<String> temp=new ArrayList<>();
+                for( int index=0;index<list.size();index++){
+                    for(int seq=0;seq<args.length;seq++){
+                        temp.add(String.format("%s,%s",list.get(index),String.valueOf(seq)));
                     }
                 }
+                list=temp;
             }
         }
-        return totalList;
+        List<String> afterFilter=new ArrayList<>();
+        //筛选
+        for(String index:list){
+            List<String> caches=new ArrayList<>();
+            String[] attrs=index.split(",");
+            long last=-1L;
+            boolean skip=false;
+            for(String strtemp:attrs){
+                if(caches.contains(strtemp)||//剔重
+                        Long.valueOf(strtemp)<last){//剔逆
+                    skip=true;
+                    break;
+                }else{
+                    caches.add(strtemp);
+                    last=Long.valueOf(strtemp);
+                }
+            }
+            if(!skip){
+                String indexArgs=null;
+                for(String attr:attrs){
+                    if(null==indexArgs){
+                        indexArgs=args[Math.toIntExact(Long.valueOf(attr))];
+                        continue;
+                    }
+                    indexArgs=String.format("%s,%s",indexArgs,args[Math.toIntExact(Long.valueOf(attr))]);
+                }
+                afterFilter.add(indexArgs);
+            }
+        }
+        return afterFilter;
     }
+
+
+
 
 
     public static void main(String[] args) {
-        String[] strs={"1","2","3","4","5"};
-        System.out.println(subListByCountV2(3,strs));
+        int max=10;
+        List<String> str=new ArrayList<>();
+        for(int i=1;i<max;i++){
+            str.add(i+"");
+        }
+        long time=System.currentTimeMillis();
+        System.out.println(getCombies(2, str.toArray(new String[str.size()])).size());
+        System.out.println(System.currentTimeMillis()-time);
     }
 
 
