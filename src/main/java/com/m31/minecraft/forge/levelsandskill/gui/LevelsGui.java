f9d855ce90c691e8a4f6b4035f8bf6502fe30d02
muunnn <hackingmu@outlook.com>
2019-08-06 01:06:00 +0800

    规范化界面写法

--- src/main/java/com/m31/minecraft/forge/levelsandskill/gui/LevelsGui.java
+++ src/main/java/com/m31/minecraft/forge/levelsandskill/gui/LevelsGui.java
@@ -3,20 +3,30 @@ package com.m31.minecraft.forge.levelsandskill.gui;
 import com.m31.minecraft.forge.levelsandskill.LevelsAndSkill;
 import com.m31.minecraft.forge.levelsandskill.utils.DataAccesser;
 import com.m31.minecraft.forge.levelsandskill.utils.TranslationUtil;
-import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiLabel;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 
 import java.io.IOException;
 
 public class LevelsGui extends GuiScreen {
+
+//    int pic_width=256;
+//    int pic_height=256;
+//    int very_start_x=(width/2)-128;
+//    int very_start_y=5;
+
+
     static final String USERNAME="user_name";
+    static final String MAXHEALTH="user_max_health";
+    static final String CURRENTHEALTH="user_curr_health";
 
 
     static final String path= LevelsAndSkill.MOD_ID+":textures/gui/gui_skill.png";
@@ -33,32 +43,22 @@ public class LevelsGui extends GuiScreen {
     @Override
     public void initGui() {
         super.initGui();
-
-//        buttonList.add(btnClose=new GuiButton(0,(int)(width*0.75), (int) (height*0.85),80,20,"Back"));
+        //name label
     }
     @Override
     public void drawScreen(int p_drawScreen_1_, int p_drawScreen_2_, float p_drawScreen_3_) {
-        int pic_width=256;
-        int pic_height=256;
-        int very_start_x=width/2-128;
-        int very_start_y=5;
-
 
         drawDefaultBackground();
         super.drawScreen(p_drawScreen_1_, p_drawScreen_2_, p_drawScreen_3_);
         GlStateManager.color(1f,1f,1f);
         this.mc.getTextureManager().bindTexture(texture);
         this.drawTexturedModalRect((width / 2)-128,5,0,0,256,256);
-        String maxHealth =DataAccesser.getPlayerMaxHealth(entityPlayerMp.getEntityData())+"";
-        //字体渲染器
-        FontRenderer fontRenderer=this.mc.fontRenderer;
-        //名称 pos  x 1/32 y 2/32
-        this.drawString(fontRenderer, TranslationUtil.getModTranslateAttrName(USERNAME)+" :"+entityPlayerMp.getDisplayNameString()
-        ,very_start_x+pic_width/32,very_start_y+pic_height/16,0XFFFFFF);
+        getNameLable().drawLabel(this.mc,0,0);
+        getMaxHealthLable().drawLabel(this.mc,0,0);
+        getMaxHealthNumLable().drawLabel(this.mc,0,0);
+        getCurrHealthLable().drawLabel(this.mc,0,0);
+        getCurrHealthNumLable().drawLabel(this.mc,0,0);
 
-
-
-        this.drawString(this.mc.fontRenderer,"max health:"+maxHealth,width / 2,height/10,0XFFFFFF);
     }
 
     @Override
@@ -69,4 +69,64 @@ public class LevelsGui extends GuiScreen {
     }
 
 
+    /**获取姓名标签
+     *
+     * @return
+     */
+    private GuiLabel getNameLable(){
+        GuiLabel target=new GuiLabel(fontRenderer,1,getVeryStartX()+getPicWidth()/32,getVeryStartY()+getPicHeight()/32,
+                getPicWidth()/8,getPicHeight()/32,0XFFFFFF);
+        target.addLine(TranslationUtil.getFullModTranslateAttrName(USERNAME)+":"+entityPlayerMp.getDisplayNameString());
+        return target;
+    }
+    /**获取姓名标签
+     *
+     * @return
+     */
+    private GuiLabel getMaxHealthLable(){
+        GuiLabel target=new GuiLabel(fontRenderer,1,getVeryStartX()+getPicWidth()/32,getVeryStartY()+((getPicHeight()/32)*10)-3,
+                getPicWidth()/16,getPicHeight()/32,0XFFFFFF);
+        target.addLine(TranslationUtil.getFullModTranslateAttrName(MAXHEALTH)+":");
+        return target;
+    }
+    private GuiLabel getMaxHealthNumLable(){
+        int maxhp=MathHelper.ceil(Minecraft.getMinecraft().player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).getAttributeValue());
+        GuiLabel target=new GuiLabel(fontRenderer,1,getVeryStartX()+(getPicWidth()/32)*7,getVeryStartY()+((getPicHeight()/32)*10)-3,
+                getPicWidth()/16,getPicHeight()/32,0XFFFFFF);
+        target.addLine("§l"+maxhp);
+        return target;
+    }
+    private GuiLabel getCurrHealthLable(){
+        GuiLabel target=new GuiLabel(fontRenderer,1,getVeryStartX()+getPicWidth()/32,getVeryStartY()+((getPicHeight()/32)*11)-1,
+                getPicWidth()/16,getPicHeight()/32,0XFFFFFF);
+        target.addLine(TranslationUtil.getFullModTranslateAttrName(CURRENTHEALTH)+":");
+        return target;
+    }
+    private GuiLabel getCurrHealthNumLable(){
+        int currenthp= MathHelper.ceil(Minecraft.getMinecraft().player.getHealth());
+        GuiLabel target=new GuiLabel(fontRenderer,1,getVeryStartX()+(getPicWidth()/32)*7,getVeryStartY()+((getPicHeight()/32)*11)-1,
+                getPicWidth()/16,getPicHeight()/32,0XFFFFFF);
+        target.addLine("§l"+currenthp);
+        return target;
+    }
+
+
+
+    private int getVeryStartX(){
+        return (width/2)-128;
+    }
+    private int getVeryStartY(){
+        return 8;
+    }
+    private int getPicWidth(){
+        return 256;
+    }
+    private int getPicHeight(){
+        return 256;
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false;
+    }
 }
--- src/main/java/com/m31/minecraft/forge/levelsandskill/utils/TranslationUtil.java
+++ src/main/java/com/m31/minecraft/forge/levelsandskill/utils/TranslationUtil.java
@@ -1,9 +1,13 @@
 package com.m31.minecraft.forge.levelsandskill.utils;
 
 import com.m31.minecraft.forge.levelsandskill.LevelsAndSkill;
+import net.minecraft.util.text.TextComponentTranslation;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TranslationUtil {
-
+    public static Map<String,String> translateKeyCache=new ConcurrentHashMap<>();
     public static String getModTranslateString(String name){
         return LevelsAndSkill.MOD_ID+"."+name;
     }
@@ -16,5 +20,23 @@ public class TranslationUtil {
     public static String getModTranslateKeyBindString(String name){
         return LevelsAndSkill.MOD_ID+".keybind."+name;
     }
+    public static String getFullModTranslateAttrName(String attrName){
+        String targetKey=getModTranslateAttrName(attrName);
+        return getTranslateKey(targetKey);
+    }
+
+    public static String getTranslateKey(String key){
+        if(translateKeyCache.containsKey(key)){
+            return translateKeyCache.get(key);
+        }else{
+            String content= new TextComponentTranslation(key).getFormattedText();
+            translateKeyCache.put(key,content);
+            return content;
+        }
+    }
+
+
+
+
 
 }
